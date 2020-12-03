e8d2486d553ee01fe5ec6940fc46bd43ad8e22a6
2dam <2dam@HZ359585.TartangaLH.eus>
2020-12-03 09:47:11 +0100

    Entity Proveedor finalizada.

--- src/java/flyshoes/entity/Proveedor.java
+++ src/java/flyshoes/entity/Proveedor.java
@@ -6,54 +6,190 @@
 package flyshoes.entity;
 
 import java.io.Serializable;
+import java.util.Set;
 import javax.persistence.Entity;
+import static javax.persistence.FetchType.EAGER;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 /**
+ * Esta clase se encargará de facilitarnos productos(ropa y zapatillas).
  *
- * @author Lorena
+ * @author Fredy
  */
 @Entity
+@Table(name = "proveedor", schema = "flyshoesdb")
 public class Proveedor implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //id del proveedor
+    private Long idProveedor;
+    //tipo de proveedor
+    private String tipo;
+    //empresa a la que pertenece el proveedor
+    private String empresa;
+    //correo del proveedor
+    private String email;
+    //telefono del proveedor
+    private String telefono;
+    //descripcion del proveedor
+    private String descripcion;
 
-    public Long getId() {
-        return id;
+    //administrador encargado del proveedor
+    @ManyToOne
+    private Administrador administrador;
+    //lista de productos ofrecidos por el proveedor
+    @OneToMany(mappedBy = "proveedor", fetch = EAGER)
+    private Set<Producto> productos;
+
+    /**
+     * Retorna el administrador encargado
+     *
+     * @return administrador
+     */
+    public Administrador getAdministrador() {
+        return administrador;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    /**
+     * Añadir administrador
+     *
+     * @param administrador
+     */
+    public void setAdministrador(Administrador administrador) {
+        this.administrador = administrador;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    /**
+     * Devuelve lista de productos ofrecidas por el proveedor
+     *
+     * @return lista de productos
+     */
+    public Set<Producto> getProductos() {
+        return productos;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Proveedor)) {
-            return false;
-        }
-        Proveedor other = (Proveedor) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    /**
+     * Añade lista de productos ofrecidas por el proveedor
+     *
+     * @param productos
+     */
+    public void setProductos(Set<Producto> productos) {
+        this.productos = productos;
     }
 
-    @Override
-    public String toString() {
-        return "flyshoes.entity.Proveedor[ id=" + id + " ]";
+    /**
+     * Devuelve el id del proveedor
+     *
+     * @return id
+     */
+    public Long getIdProveedor() {
+        return idProveedor;
     }
-    
+
+    /**
+     * Inserta el id del proveedor
+     *
+     * @param idProveedor
+     */
+    public void setIdProveedor(Long idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    /**
+     * Devuelve el tipo de proveedor
+     *
+     * @return tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * Inserta el tipo de proveedor
+     *
+     * @param tipo
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * Devuelve el nombre de la empresa ala que pertenece el proveedor
+     *
+     * @return empresa
+     */
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    /**
+     * Inserta el nombre de la empresa ala que pertenece el proveedor
+     *
+     * @param empresa
+     */
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    /**
+     * Devuelve el correo del proveedor
+     *
+     * @return correo
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Inserta el correo del proveedor
+     *
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Devuel el telefono del proveedor
+     *
+     * @return telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * Inserta el telefono del proveedor
+     *
+     * @param telefono
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * Devuelve la descripcion del proveedor
+     *
+     * @return descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * Inserta la descripcion del proveedor
+     *
+     * @param descripcion
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
