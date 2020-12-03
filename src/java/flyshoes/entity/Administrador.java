8ced0595880c6039ba48f0c5d9b2ea1c4c1b2479
2dam <2dam@HZ359585.TartangaLH.eus>
2020-12-03 09:49:28 +0100

    Entity Administrador finalizada.

---
+++ src/java/flyshoes/entity/Administrador.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flyshoes.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.Entity;
+import static javax.persistence.FetchType.EAGER;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+/**
+ * Clase Administrador que hereda de Usuario, esta clase será la encargada de la
+ * gestion de proveedores.
+ *
+ *
+ * @author Fredy
+ */
+@Entity
+@PrimaryKeyJoinColumn(referencedColumnName = "id")
+public class Administrador extends Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    //Lista de Proveedores gestionados
+    @OneToMany(mappedBy = "administrador", fetch = EAGER)
+    private Set<Proveedor> proveedores;
+
+    /**
+     * Devuelve la lista de proveedores gestionados
+     *
+     * @return proveedores
+     */
+    public Set<Proveedor> getProveedores() {
+        return proveedores;
+    }
+
+    /**
+     * Inserta una lista de proveedores
+     *
+     * @param lista de proveedores
+     */
+    public void setProveedores(Set<Proveedor> proveedores) {
+        this.proveedores = proveedores;
+    }
+
+}
