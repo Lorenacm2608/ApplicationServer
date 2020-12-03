c223f5a16b0fe5d964b92ae82aaf603d9c40bb32
2dam <2dam@HZ359585.TartangaLH.eus>
2020-12-03 09:43:54 +0100

    Entity Reserva finalizada.

--- src/java/flyshoes/entity/Reserva.java
+++ src/java/flyshoes/entity/Reserva.java
@@ -11,49 +11,152 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import static javax.persistence.FetchType.EAGER;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 /**
+ * Esta clase se ecncargará sobre la gestión de reservas
  *
- * @author Lorena
+ * @author Fredy
  */
 @Entity
+@Table(name = "reserva", schema = "flyshoesdb")
 public class Reserva implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    //id de la reserva
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    //lista de productos que contiene la reserva
+    @OneToMany(mappedBy = "reserva", fetch = EAGER)
+    private List<Producto> productos;
+    //descripcion de la reserva
+    private String descripcion;
+    //estado de la reserva
+    private EstadoReserva estado;
+    //vendedor que gestiona la reserva
+    @ManyToOne
+    private Vendedor vendedor;
+    //cliente dueño de la reserva
+    @ManyToOne
+    private Cliente cliente;
 
+    /**
+     * Devuelve el id de la reserva
+     *
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Inserta el id de la reserva
+     *
+     * @param id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    /**
+     * Devuelve la descripcion de la reserva
+     *
+     * @return descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Reserva)) {
-            return false;
-        }
-        Reserva other = (Reserva) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    /**
+     * Inserta la descripcion de la reserva
+     *
+     * @param descripcion
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 
-    @Override
-    public String toString() {
-        return "flyshoes.entity.Reserva[ id=" + id + " ]";
+    /**
+     * Devuelve el estado de la reserva
+     *
+     * @return estado
+     */
+    public EstadoReserva getEstado() {
+        return estado;
     }
-    
+
+    /**
+     * Inserta el estado de la reserva
+     *
+     * @param estado
+     */
+    public void setEstado(EstadoReserva estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * Devuelve el cliente que ha realizado dicha reserva
+     *
+     * @return cliente
+     */
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    /**
+     * Añade al cliente de la reserva
+     *
+     * @param cliente
+     */
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * Devuelve al vendedor que ha gestionado la reserva
+     *
+     * @return vendedor
+     */
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    /**
+     * Inserta al vendedor de la reserva
+     *
+     * @param vendedor
+     */
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    /**
+     * Devuelve la lista de productos que contiene la reserva
+     *
+     * @return productos
+     */
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    /**
+     * Añade una lista de productos que contiene la reserva
+     *
+     * @param productos
+     */
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
 }
