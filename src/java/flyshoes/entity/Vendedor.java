bfec9db6ea7aed965e77ce3c2a05b436a80e0eba
pocoy <pocoy@DESKTOP-65ERJ1P>
2020-12-09 16:24:04 +0100

    Agregado javadoc

--- src/java/flyshoes/entity/Vendedor.java
+++ src/java/flyshoes/entity/Vendedor.java
@@ -65,52 +65,99 @@ public class Vendedor extends Usuario implements Serializable {
         
     }
 
+    /*
+     * Devuelve el valor de salario para Usuario
+     * @return el valor de salario
+     */
     public Integer getSalario() {
         return salario;
     }
 
+    /*
+     * Establece el valor de salario para Usuario
+     * @param de salario del valor salario
+     */
     public void setSalario(Integer salario) {
         this.salario = salario;
     }
 
+    /*
+     * Devuelve el valor de dni para Usuario
+     * @return el valor de dni
+     */
     public String getDni() {
         return dni;
     }
-
+    /*
+     * Establece el valor de dni para Usuario
+     * @param de dni del valor dni
+     */
     public void setDni(String dni) {
         this.dni = dni;
     }
 
+    /*
+     * Devuelve el valor de direccion para Usuario
+     * @return el valor de direccion
+     */
     public String getDireccion() {
         return direccion;
     }
 
+    /*
+     * Establece el valor de direccion para Usuario
+     * @param de direccion del valor direccion
+     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    /*
+     * Devuelve el valor de tienda para Usuario
+     * @return el valor de tienda
+     */
     public String getTienda() {
         return tienda;
     }
 
+    /*
+     * Establece el valor de tienda para Usuario
+     * @param de tienda del valor tienda
+     */
     public void setTienda(String tienda) {
         this.tienda = tienda;
     }
     
+    /*
+     * Devuelve el valor de cliente para Usuario
+     * @return el valor de cliente
+     */
     @XmlTransient
     public Set<Cliente> getCliente() {
         return clientes;
     }
 
+    /*
+     * Establece el valor de cliente para Usuario
+     * @param de cliente del valor cliente
+     */
     public void setCliente(Set<Cliente> clientes) {
         this.clientes = clientes;
     }
 
+    /*
+     * Devuelve el valor de producto para Usuario
+     * @return el valor de producto
+     */
     @XmlTransient
     public Set<Producto> getProducto() {
         return productos;
     }
 
+    /*
+     * Establece el valor de producto para Usuario
+     * @param de producto del valor producto
+     */
     public void setProducto(Set<Producto> producto) {
         this.productos = productos;
     }
