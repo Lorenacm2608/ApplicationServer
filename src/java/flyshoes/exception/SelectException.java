3f185715e0e44abf082db79f47a400f2fccf22f9
Lorenacm2610 <lorena.manuel2610@gmail.com>
2020-12-11 12:01:39 +0100

    Excepciones

---
+++ src/java/flyshoes/exception/SelectException.java
@@ -0,0 +1,25 @@
+package flyshoes.exception;
+
+/**
+ * Excepción que se lanza cuando ocurre algun problema a la hora de leer
+ *
+ * @author 2dam
+ */
+public class SelectException extends Exception {
+
+    /**
+     * Constructor vacio
+     */
+    public SelectException() {
+    }
+
+    /**
+     * Constructor el cual recibe un mensaje detallado
+     *
+     * @param message
+     */
+    public SelectException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/java/flyshoes/exception/UpdateException.java
@@ -0,0 +1,25 @@
+package flyshoes.exception;
+
+/**
+ * Excepción que se lanza cuando ocurre algun problema a la hora de actualizar
+ *
+ * @author 2dam
+ */
+public class UpdateException extends Exception {
+
+    /**
+     * Constructor vacío
+     */
+    public UpdateException() {
+    }
+
+    /**
+     * Constructor el cual recibe un mensaje detallado
+     *
+     * @param message
+     */
+    public UpdateException(String message) {
+        super(message);
+    }
+
+}
